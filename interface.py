b8a70566413aeb9a13c886757af2101015426983
Daniel Monastirski <danielmonas.12@gmail.com>
2019-02-09 14:44:04 +0200

    Began work on GUI

--- interface.py
+++ interface.py
@@ -1,37 +1,9 @@
-""" User interface for the Gmail Sorter """
+""" UI for the entire project, using Kivy """
 
-import sys
-import os
-from tagger import Tagger
-
-FLAGS = {
-    "settings": "-s",
-    "Gmail": "-g",
-    "time": "-t"
-}
+import kivy
 
 def main():
     """ Main function. """
-    tagger = Tagger()
-    arguments = sys.argv[1:]
-    days = "2"
-    if not any(x in arguments for x in list(FLAGS.values())):
-        print("Usage: {0} [OPTIONS]".format(os.path.basename(__file__)))
-        print("{0}: Edit the Gmail settings file.".format(FLAGS["settings"]))
-        print("{0}: Run the Gmail tagging script.".format(FLAGS["Gmail"]))
-        print("{0}=DAYS: Filter by number of days, by default 2 days.".format(FLAGS["time"]))
-        return
-
-    if FLAGS["time"] in arguments:
-        if FLAGS["time"] == arguments[-1]:
-            print("Error: Invalid flag!")
-            return
-        days = arguments[arguments.index(FLAGS["time"]) + 1]
-
-    if FLAGS["settings"] in arguments:
-        tagger.config.edit()
-    if FLAGS["Gmail"] in arguments:
-        tagger.init_queries(days)
-
+    print("Test")
 if __name__ == '__main__':
     main()
